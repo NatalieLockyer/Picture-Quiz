df73565f9eab3eaa3e029f3e3da2fe6de5eae3f9
Natalie Lockyer <Natalie_lockyer@outlook.com>
2023-09-22 13:22:19 +0000

    Easy questions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,75 @@ let easyQuestions = [
     {
         q: 1,
         //Image to go at top of question//
-        img: "/assets/images/easy-questions/cards.webp"
-        question: "How many cards are there in a standard pack of playing cards"
-        correctAnswer: "52"
-        wrongAnswers: ["50", "48", "54"]
-
-
+        img: "/assets/images/easy-questions/cards.webp",
+        question: "How many cards are there in a standard pack of playing cards",
+        correctAnswer: "52",
+        incorrectAnswers: ["50", "48", "54"],
+    },
+    {
+        q: 2,
+        img: "/assets/images/easy-questions/covidvaccine.webp",
+        question: "When was the first Covid vaccine invented?",
+        correctAnswer: "2020",
+        incorrectAnswers: ["2019", "2021", "2022"],
+    },
+    {
+        q: 3,
+        img: "/assets/images/easy-questions/dubai.jpg",
+        question: "Where in the world would you find this statue?",
+        correctAnswer: "Dubai",
+        incorrectAnswers: ["Saudi Arabia", "Qatar", "Bahrain"],
+    },
+    {
+        q: 4,
+        img: "/assets/images/easy-questions/labradorpuppy.webp",
+        question: "Can you identify what type of dog is this?",
+        correctAnswer: "Labrador",
+        incorrectAnswers: ["Poodle", "Spaniel", "Retriever"],
+    },
+    {
+        q: 5,
+        img: "/assets/images/easy-questions/lightbulb.webp",
+        question: "What year was the first light bulb invented?",
+        correctAnswer: "1879",
+        incorrectAnswers: ["1900", "1854", "1895"],
+    },
+    {
+        q: 6,
+        img: "/assets/images/easy-questions/monopoly.jpg",
+        question: "In Monopoly, what is the most expensive property?",
+        correctAnswer: "Mayfair",
+        incorrectAnswers: ["Pall Mall", "Regents Road", "Old Kent Road"],
+    },
+    {
+        q: 7,
+        img: "/assets/images/easy-questions/mosalah.webp",
+        question: "Which football team does Mo Salah currently play for?",
+        correctAnswer: "Liverpool"
+        incorrectAnswers: ["Manchester United", "Newcastle United", "Arsenal"]
+    },
+    {
+        q: 8,
+        img: "/assets/images/easy-questions/newyork.webp",
+        question: "Where in the world would you find this statue?",
+        correctAnswer: "New York",
+        incorrectAnswers: ["Madrid", "Paris", "California"],
+    },
+    {
+        q: 9,
+        img: "/assets/images/easy-questions/paris.jpg",
+        question: "The Eiffel Tower is situated in which country?",
+        correctAnswer: "France",
+        incorrectAnswers: ["England", "Germany", "Spain"],
+    },
+    {
+        q: 10,
+        img: "/assets/images/easy-questions/sanfrancisco.webp",
+        question: "How long, in meters, is the Golden Gate Bridge?"
+        correctAnswer: "2737meters",
+        incorrectAnswers: ["1503meters", "2212meters", "1778meters"],
     }
+    
+
+
 ]
