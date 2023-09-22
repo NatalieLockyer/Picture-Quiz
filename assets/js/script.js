0a7a32f05bcfd1b16aeb66f4638aeea54de0bca4
Natalie Lockyer <Natalie_lockyer@outlook.com>
2023-09-22 09:21:50 +0000

    Starting to upload questions on JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 //This are the Questions for each category - staring with Easy Questions
 let easyQuestions = [
     {
-        q:1,
-        
+        q: 1,
+        //Image to go at top of question//
+        img: "/assets/images/easy-questions/cards.webp"
+        question: "How many cards are there in a standard pack of playing cards"
+        correctAnswer: "52"
+        wrongAnswers: ["50", "48", "54"]
+
+
     }
 ]
